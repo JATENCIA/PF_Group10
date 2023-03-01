7110c019bbcc6012c7667d88faf182a280eef17a
Maximiliano Pogonza <pogonza00@gmail.com>
2023-03-01 09:15:27 -0300

    cambios en app y index para modularizar

--- server/src/Routes/Users.js
+++ server/src/Routes/Users.js
@@ -1,9 +1,7 @@
-const express = require("express");
-const router = express.Router();
+const { Router } = require("express");
+const router = Router();
 const { routerGetUser } = require("../Controllers/usersController");
 
-router.get("/", (req, res) => {
-  routerGetUser(req, res);
-});
+router.get("/", routerGetUser);
 
 module.exports = router;
